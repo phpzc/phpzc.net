74ed039062d763e89dbfd81c85f4bf2c10e7a662
张成 <zhang5474jj@163.com>
2018-11-19 17:46:07 +0800

    update cache dir

--- app/Http/Controllers/Web/CommonController.php
+++ app/Http/Controllers/Web/CommonController.php
@@ -299,7 +299,7 @@ class CommonController extends Controller
      */
     public function getCacheArray($path, $name)
     {
-        $file = './storage/app/public';
+        $file = './storage/app/public/';
         $target = $file . $path . '/' .sha1($name) . '.php';
         $fpath = $file . $path;
         if (!is_dir($fpath)) {
@@ -340,7 +340,7 @@ class CommonController extends Controller
      */
     public function saveCacheArray($path, $name, $data)
     {
-        $file = './storage/app/public';
+        $file = './storage/app/public/';
         $target = $file . $path . '/' . sha1($name) . '.php';
         $fpath = $file . $path;
         if (!is_dir($fpath)) {
