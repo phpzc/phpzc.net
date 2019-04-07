bbda6fafcfe74393f46e5d4f45025e8f1d3d63fc
phpzc <zhang5474jj@163.com>
2019-04-07 12:12:41 +0800

    add menu project blade

--- resources/views/layouts/layout2.blade.php
+++ resources/views/layouts/layout2.blade.php
@@ -122,33 +122,7 @@
                             </li>
 
 
-                            <!--添加project文章 -->
-                            @foreach ($MENU_PROJECT as $project_data)
-
-                                <li class="nav-item dropdown"  >
-                                    <a href="javascript:void(0)" class="nav-link @if ($THIS_PROJECT_ID == $project_data['project_id'])
-                                            active
-                                    @endif" data-toggle="dropdown"><i class="fe  fe-file-text"></i>{{ $project_data['name'] or '' }} </a>
-                                    <div class="dropdown-menu dropdown-menu-arrow">
-
-                                        @foreach($project_data['summary'] as $summary_data)
-
-
-                                            <?php foreach ($summary_data['sub_data'] as $article_data){ ?>
-
-
-                                            <a href="/project/detail?id={{ $article_data['id'] or '' }}" class='@if ($this_id == $article_data['id']) active @endif dropdown-item' >
-                                                <b>{{ $article_data['title'] }}</b>
-                                            </a>
-
-                                            <?php } ?>
-
-                                        @endforeach
-
-                                    </div>
-
-                                </li>
-                            @endforeach
+                            <!--添加project文章 menu_project -->
 
 
                             <li class="nav-item">
